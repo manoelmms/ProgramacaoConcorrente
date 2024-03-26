d7551782f0417c92037422c6f02d9ee9f16f7002
manoelmms <manoelmarcelomms@gmail.com>
2024-03-26 18:41:43 -0300

    Long int vector size fix and some comments added

--- Laboratório 1/atv_5.c
+++ Laboratório 1/atv_5.c
@@ -3,17 +3,17 @@
 #include <pthread.h>
 #include <time.h>
 
-#define MAX 1000
-#define TAM 100000
-#define NTHREADS 79
+#define MAX 100
+#define TAM 10000
+#define NTHREADS 23
 
 typedef struct {
     int idThread, nThreads;
     int *start_vet;
-    int limit;
+    long int limit;
 } t_Args;
 
-int* init_vet(int tam){
+int* init_vet(long int tam){
     int *vetor = (int *) malloc(sizeof(int) * tam);
     if (vetor == NULL){
         printf("--ERRO: malloc()\n"); exit(-1);
@@ -22,44 +22,44 @@ int* init_vet(int tam){
     
 }
 
-void rand_vet(int *vetor, int tam){
+void rand_vet(int *vetor, long int tam){
     srand(time(NULL));
-    for (int i = 0; i < tam; i++){
+    for (long int i = 0; i < tam; i++){
         long int number = rand() % (2*MAX) - MAX; // Random number between -MAX and MAX
         vetor[i] = number;
     }
 }
 
-
-void seq_vet(int *vetor, int tam){
-    for (int i = 0; i < tam; i++){
+void seq_vet(int *vetor, long int tam){
+    for (long int i = 0; i < tam; i++){
         vetor[i] = i + 1;
     }
 }
 
-void* duplicate_vet(int *vetor, int tam){
+void* duplicate_vet(int *vetor, long int tam){
     int *new_vet = (int *) malloc(sizeof(int) * tam);
     if (new_vet == NULL){
         printf("--ERRO: malloc()\n"); exit(-1);
     }
-    for (int i = 0; i < tam; i++){
+    for (long int i = 0; i < tam; i++){
         new_vet[i] = vetor[i];
     }
     return (void *) new_vet;
     
 }
 
-void print_vet(int *vet, int tam){
-    for (int i = 0; i < tam; i++){
+void print_vet(int *vet, long int tam){
+    for (long int i = 0; i < tam; i++){
         printf("%d ", vet[i]);
     }
     printf("\n");
 }
 
-int testa_result(int *original, int *result, int tam){
-    for (int i = 0; i < tam; i++){
+int testa_result(int *original, int *result, long int tam){
+    // Sequential algorithm to compare the results
+    for (long int i = 0; i < tam; i++){
         if (result[i] != original[i] * original[i]){
-            printf("Erro na posição %d\n", i);
+            printf("Erro na posição %ld\n", i);
             printf("> Original: %d\n ", original[i]);
             printf("> Resultado: %d\n ", result[i]);
             return 0;
@@ -68,10 +68,9 @@ int testa_result(int *original, int *result, int tam){
     return 1;
 }
 
-
 void* quad_vet(void *arg){
     t_Args args = *(t_Args*) arg;
-    for (int i = 0; i < args.limit; i++){
+    for (long int i = 0; i < args.limit; i++){
         args.start_vet[i] = args.start_vet[i] * args.start_vet[i];
         printf("Sou a thread %d de %d threads e meu resultado é %d\n", args.idThread + 1, args.nThreads, args.start_vet[i]);
     }
@@ -80,7 +79,7 @@ void* quad_vet(void *arg){
 }
 
 int main(){
-
+    // Initializing data structures
     int *vetor = init_vet(TAM);
     rand_vet(vetor, TAM);
     //seq_vet(vetor, TAM);
@@ -92,6 +91,12 @@ int main(){
     pthread_t tid[NTHREADS];
     t_Args *arg;
 
+    //Warning in case of NTHREADS > TAM
+    if (NTHREADS > TAM){
+        printf(">Warning!!!: NTHREADS > TAM\n"); // Still runs without problem, but the threads will be idle
+    }
+    
+    // Creating threads
     for (int i = 0; i < NTHREADS; i++){
         arg = malloc(sizeof(t_Args));
         if (arg == NULL){
@@ -109,7 +114,7 @@ int main(){
             arg->limit = TAM / NTHREADS;
         }
 
-        arg->start_vet = vetor + i * (TAM / NTHREADS);
+        arg->start_vet = vetor + i * (TAM / NTHREADS); // Pointer to the beginning of the part of the vector that the thread will work on
 
 
         if (pthread_create(&tid[i], NULL, quad_vet, (void *) arg)){
@@ -123,8 +128,8 @@ int main(){
         }
     }
 
-    // printf("\nResultado: ");
-    // print_vet(vetor, TAM);
+    printf("\nResultado: ");
+    print_vet(vetor, TAM);
 
     if (testa_result(test, vetor, TAM)){
         printf("\nVetor original foi alterado corretamente\n");
